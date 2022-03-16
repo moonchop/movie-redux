5eae5f0bb45bfc0612ed084eee2680aad06c27ea
Moonchop <82392767+moonchop@users.noreply.github.com>
2022-03-16 16:15:33 +0900

    refactoring redux-practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
-import Fetch from './components/Fetch';
-import CounterContainer from './containers/CounterContainer';
+import CounterContainer from './redux-practice/containers/CounterContainer';
 function App() {
   return (
     <div className="App">
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import { createStore } from 'redux';
-import rootReducer from './modules';
+import rootReducer from './redux-practice/modules';
 import { Provider } from 'react-redux';
 
 const store = createStore(rootReducer); // 스토어를 만듭니다.
